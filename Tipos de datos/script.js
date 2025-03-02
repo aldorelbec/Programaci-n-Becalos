17bdf14d26837972b8ac4bebc08690ee72013233
Aldo <aldourielbec@gmail.com>
2025-03-02 12:23:56 -0600

    agregamos git ignore

---
+++ Tipos de datos/script.js
@@ -0,0 +1,59 @@
+/* 
+Tipos de datos en JS
+*/
+
+//Tipo number (numero)
+
+var edad = 25 
+var precio = 500*10
+var puntos = 0 
+
+ // Tipo string (Texto)
+
+ var nombre = "Sheyla "
+ var saludo= "Hola ¿Como estas?"
+ var character = "a"
+ var numerocadena= "25"
+
+//Tipo booleano
+
+var esmayor= true 
+var tienepermiso= false
+
+// Tipo Symbol
+
+var identificarid= Symbol("id")
+var identificador2= Symbol("id")
+
+//Tipo bigInt 
+var temperatura= 23423423432432423432n /*se identifica con una "n" al final
+y solo se pueden operar bigInts con BigInts*/
+
+
+//Tipo null
+var vacio= null
+
+//Tipo Undefined
+var sindefinir
+
+//Tipo NaN (Not a number) cuando se operan numeros con letras
+
+var resultadonovalido= "hola"*3
+
+/* Console.log:
+se usa para imprimir valores en la 
+consola del navegador
+
+*/
+console.log (edad)
+console.log (nombre)
+
+//¿Que es Typeof:
+// Typeof se usa para conocer que tipo de datode una variable
+//Es util para depuración y validación de codigo+
+console.log(typeof edad)
+console.log(typeof numerocadena)
+
+
+
+
