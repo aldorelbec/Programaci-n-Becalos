05cf84d62447dd89f7b1baa1998b9d9355507d23
Aldo <aldourielbec@gmail.com>
2025-03-04 14:01:33 -0600

    Proyecto 2 evaluador de notas

---
+++ Comparaciones y decisiones en JavaScript/script.js
@@ -0,0 +1,62 @@
+// Evaluador de Notas con Mensajes Personalizados
+
+var nota = 98
+
+if (nota >= 90 ){
+    console.log ("Exelente :))")
+}
+else if(nota >= 75 && nota <= 89) {
+    console.log ("Bien :) ")
+}
+else if (nota >= 60 && nota <= 74){
+    console.log ("Suficiente :|")
+}
+else {
+    console.log ("Reprobado ;(")
+}
+/////////////////////////////////////////////////////
+var nota = 87
+
+if (nota >= 90 ){
+    console.log ("Exelente :))")
+}
+else if(nota >= 75 && nota <= 89) {
+    console.log ("Bien :) ")
+}
+else if (nota >= 60 && nota <= 74){
+    console.log ("Suficiente :|")
+}
+else {
+    console.log ("Reprobado ;(")
+}
+////////////////////////////////////////////////
+var nota = 72
+
+if (nota >= 90 ){
+    console.log ("Exelente :))")
+}
+else if(nota >= 75 && nota <= 89) {
+    console.log ("Bien :) ")
+}
+else if (nota >= 60 && nota <= 74){
+    console.log ("Suficiente :|")
+}
+else {
+    console.log ("Reprobado ;(")
+}
+////////////////////////////////////////////////////
+var nota = 5
+
+if (nota >= 90 ){
+    console.log ("Exelente :))")
+}
+else if(nota >= 75 && nota <= 89) {
+    console.log ("Bien :) ")
+}
+else if (nota >= 60 && nota <= 74){
+    console.log ("Suficiente :|")
+}
+else {
+    console.log ("Reprobado ;(")
+}
+///////////////////////////////////////////////////////
